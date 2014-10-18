2dcc1f8e05d41da6a8def0524c20900e519ca1eb
leventov <leventov@ya.ru>
2014-10-19 03:13:02 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+## A Maven plugin for [JPSG](https://github.com/OpenHFT/Koloboke/tree/master/jpsg)
+
+### Quick start
+
+1. Add to you `pam.xml`:
+    ```xml
+        <build>
+            <plugins>
+                <!-- other plugins... -->
+                
+                <plugin>
+                    <groupId>net.openhft</groupId>
+                    <artifactId>jpsg-maven-plugin</artifactId>
+                    <version>1.0</version>
+                    <executions>
+                        <execution>
+                            <id>jpsg</id>
+                            <goals>
+                                <goal>jpsg</goal>
+                            </goals>
+                        </execution>
+                    </executions>
+                </plugin>
+            </plugins>
+        </build>
+    ```
+    
+2. Create file `IntIterator.java` in `src/main/javaTemplates/test` directory:
+    ```java
+    package test;
+    
+    import java.util.Iterator;
+    
+    public interface CharIterator extends Iterator<Character> {
+    
+        char nextChar();
+    }
+    ```
+    
+3. Run `mvn compile`
+
+4. [For IntelliJ users] Right click on `pom.xml` in project explorer &rarr; Maven &rarr; Reimport
+
+5. Open `target/generated-sources/jpsg/test` directory in project explorer and enjoy
+`ByteIterator`..`DoubleIterator` specializations!
---
+++ src/main/java/net/openhft/jpsg/GeneratorMojo.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2014 Higher Frequency Trading
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.openhft.jpsg;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Mojo(name = "jpsg", defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        requiresDependencyResolution = ResolutionScope.COMPILE, requiresProject = true)
+public class GeneratorMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "SHORT")
+    protected ObjectType.IdentifierStyle objectIdStyle;
+
+    @Parameter
+    protected String defaultTypes;
+
+    @Parameter
+    protected List<String> with;
+
+    @Parameter
+    protected List<String> processors;
+
+    @Parameter
+    protected List<String> never;
+
+    @Parameter
+    protected List<String> include;
+
+    @Parameter
+    protected List<String> exclude;
+
+    @Parameter(property = "project", required = true, readonly = true)
+    protected MavenProject project;
+
+    @Parameter(defaultValue = "${basedir}/src/main/javaTemplates")
+    private File sourceDirectory;
+
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources/jpsg")
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        Generator g = new Generator();
+
+        g.setObjectIdStyle(objectIdStyle);
+        if (defaultTypes != null && !defaultTypes.isEmpty())
+            g.setDefaultTypes(defaultTypes);
+        if (with != null)
+            g.with(with);
+        if (processors != null){
+            for (String processor : processors) {
+                g.addProcessor(processor);
+            }
+        }
+        if (never != null)
+            g.never(never);
+        if (include != null)
+            g.include(include);
+        if (exclude != null)
+            g.exclude(exclude);
+        g.setSource(sourceDirectory);
+        g.setTarget(outputDirectory);
+
+        try {
+            g.generate();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error while JPSG generation", e);
+        }
+
+        if (project != null) {
+            project.addCompileSourceRoot(outputDirectory.getPath());
+        }
+    }
+}
